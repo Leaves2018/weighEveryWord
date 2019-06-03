34fa265f773842529e7e7898249847da8e0cbad0
leaves <284969173@qq.com>
2019-06-03 17:09:37 +0800

    将update.py文件中的文件打开方法指定为UTF-8编码，以尝试解决Windows查看时中文时乱码问题。

--- update.py
+++ update.py
@@ -1,10 +1,10 @@
 def update_vocabulary_words(new_words):
-    with open("./vocabulary/vocabulary_words.txt", 'a+') as vw:
+    with open("./vocabulary/vocabulary_words.txt", 'a+', encoding='UTF-8') as vw:
         for word in new_words:
             vw.write("\n" + word.to_string())
 
 
 def update_familiar_words(old_words):
-    with open("./familiar/familiar_words.txt", 'a+') as fw:
+    with open("./familiar/familiar_words.txt", 'a+', encoding='UTF-8') as fw:
         for word in old_words:
             fw.write("\n" + word)
