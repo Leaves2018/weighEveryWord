03db46eb764b10dca61227f4215921731a589352
Mingyue <1051371520@qq.com>
2019-06-08 11:05:36 +0800

    加入了爬取音标的方法

--- youdao_reptile.py
+++ youdao_reptile.py
@@ -15,36 +15,50 @@ def get_page(url):
         b = a.text
         return b
     except:
-        return ''
+        return ""
+
+
+# 获取网页上的单词的英式音标
+def deal_page_yb(url):
+    page = get_page(url)
+    bs = BeautifulSoup(page, 'html.parser')
+    try:
+        span = bs.find('span', class_='phonetic')
+        if span is not None:
+         return span.contents[0]
+    except:
+        return ""
 
 
 # 获取网页上的单词的中文解释
 def deal_page_ch(url):
     page = get_page(url)
     bs = BeautifulSoup(page, 'html.parser')
-    div = bs.find('div', class_="trans-container")
-    if div is not None:
-        for i in div:
-            s = str(type(i))
-            if func(s) == 'bs4.element.Tag':
-                t = i.find('li')
-                if t is not None:
-                    return t.contents[0]
-                else:
-                    return None
-    else:
-        return ""
+    try:
+        div = bs.find('div', class_="trans-container")
+        if div is not None:
+            for i in div:
+                s = str(type(i))
+                if func(s) == 'bs4.element.Tag':
+                    t = i.find('li')
+                    if t is not None:
+                        return t.contents[0]
+                    else:
+                        return None
+    except:
+            return ""
 
 
 def deal_page_example(url):
     page = get_page(url)
     bs = BeautifulSoup(page, 'html.parser')
-    div = bs.find('div', id='authority')
-    if div is not None:
-        li=div.find('li')
-        p=li.find('p')
-        return p.get_text()
-    else:
+    try:
+        div = bs.find('div', id='authority')
+        if div is not None:
+            li = div.find('li')
+            p = li.find('p')
+            return p.get_text()
+    except:
         return ""
 
 
@@ -52,23 +66,31 @@ def deal_page_example(url):
 def deal_page_en(url):
     page = get_page(url)
     bs = BeautifulSoup(page, 'html.parser')
-    span = bs.find('span', class_='def')
-    if span is not None:
-        return span.contents[0]
-    else:
+    try:
+        span = bs.find('span', class_='def')
+        if span is not None:
+            return span.contents[0]
+    except:
         return ""
 
 
 # 获取单词的中文意思
 def ch_mean(word):
-    return deal_page_ch('http://dict.youdao.com/w/eng/' + word + '/#keyfrom=dict2.index')
+    if word:
+        return deal_page_ch('http://dict.youdao.com/w/eng/' + word + '/#keyfrom=dict2.index')
 
 
 # 获取单词的英文意思
 def en_mean(word):
-    return deal_page_en('http://dict.youdao.com/w/eng/' + word + '/#keyfrom=dict2.index')
+    if word:
+        return deal_page_en('http://dict.youdao.com/w/eng/' + word + '/#keyfrom=dict2.index')
 
 
 def example_mean(word):
     return str(deal_page_example('http://dict.youdao.com/w/eng/' + word + '/#keyfrom=dict2.index')).strip()
+
+
+def yb_mean(word):
+    if word:
+        return deal_page_yb('http://dict.youdao.com/w/eng/' + word + '/#keyfrom=dict2.index')
 
